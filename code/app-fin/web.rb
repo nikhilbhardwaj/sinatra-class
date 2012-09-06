f6f548da0ccc1131f279f1c6c837ea8654fa9b33
Nikhil Bhardwaj <root@NikhilBhardwaj.in>
2012-09-06 11:34:44 +0530

    Finished with the first app!

--- code/app-fin/web.rb
+++ code/app-fin/web.rb
@@ -3,5 +3,5 @@ require File.dirname(__FILE__) + '/birthday_countdown.rb'
 
 get '/' do
   @countdown = BirthdayCountdown.new(9,30)
-  erb @countdown.to_view #, layout: (:layout if request.xhr?)
+  erb @countdown.to_view, :layout => (request.xhr? ? false : :layout)
 end 
